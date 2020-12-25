bd07231af0feb348faba28db7ddd974cfeaea0de
AA_Arnold <amasoharnold@gmail.com>
2020-12-25 10:56:07 -1100

    added profile picture

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
 
-
 //using the statelesswidget allows you to hot reload
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.purple[400],
         appBar: AppBar(
-          centerTitle:false,
-          toolbarOpacity:1.0,
+          centerTitle: false,
+          toolbarOpacity: 1.0,
           title: const Text("Arnold's Business Card"),
           backgroundColor: Colors.purpleAccent,
+        ),
+        body: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/arnold.jpeg'),
+            ),
+            Container(
+              child: Text(
+                'Amasoh.A.Arnold',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
