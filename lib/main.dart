f501a9f3b500254fd262ab06f51fed33984e8d95
AA_Arnold <amasoharnold@gmail.com>
2021-01-05 08:31:01 -1100

    added font, made use of card widget, project completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,10 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.purpleAccent,
         ),
         body: Column(
-          children: [
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             CircleAvatar(
-              radius: 50.0,
+              radius: 70.0,
               backgroundImage: AssetImage('assets/arnold.jpeg'),
             ),
             Container(
@@ -27,12 +28,69 @@ class MyApp extends StatelessWidget {
                 'Amasoh.A.Arnold',
                 style: TextStyle(
                     fontFamily: 'DancingScript',
-                    fontSize: 20.0,
+                    fontSize: 25.0,
+                    letterSpacing: 1.0,
                     color: Colors.white,
-                    fontWeight: FontWeight.bold
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              child: Text(
+                'Software Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.purple.shade100,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.purple.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.purple[400],
+                  ),
+                  title: Text(
+                    '0802-818-6162',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.purple.shade400,
+                    ),
+                  ),
                 ),
               ),
             ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+              child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.purple[400],
+                    ),
+                    title: Text(
+                      'amasoharnold@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.purple.shade400,
+                      ),
+                    ),
+                  )),
+            )
           ],
         ),
       ),
